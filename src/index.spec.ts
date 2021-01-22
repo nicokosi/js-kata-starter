455803de4946736b46a6b6939c18cbfc15216308
Nicolas Kosinski <nicokosi@yahoo.com>
2021-01-22 16:31:10 +0100

    FizzBuzz with several "if"

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,26 @@
-import { systemUnderTest } from ".";
+import { fizzbuzz } from ".";
 
-describe("Test of systemUnderTest()", function() {
-  it("Should pass", function() {
-    expect(systemUnderTest()).toEqual(true);
+describe("FizzBuzz", function () {
+  it("should pass", function () {
+    expect(fizzbuzz(1)).toEqual("1");
+    expect(fizzbuzz(2)).toEqual("2");
+    expect(fizzbuzz(3)).toEqual("Fizz");
+    expect(fizzbuzz(4)).toEqual("4");
+    expect(fizzbuzz(5)).toEqual("Buzz");
+    expect(fizzbuzz(6)).toEqual("Fizz");
+    expect(fizzbuzz(7)).toEqual("7");
+    expect(fizzbuzz(8)).toEqual("8");
+    expect(fizzbuzz(9)).toEqual("Fizz");
+    expect(fizzbuzz(10)).toEqual("Buzz");
+    expect(fizzbuzz(11)).toEqual("11");
+    expect(fizzbuzz(12)).toEqual("Fizz");
+    expect(fizzbuzz(13)).toEqual("13");
+    expect(fizzbuzz(14)).toEqual("14");
+    expect(fizzbuzz(15)).toEqual("FizzBuzz");
+    expect(fizzbuzz(16)).toEqual("16");
+    expect(fizzbuzz(17)).toEqual("17");
+    expect(fizzbuzz(18)).toEqual("Fizz");
+    expect(fizzbuzz(19)).toEqual("19");
+    expect(fizzbuzz(20)).toEqual("Buzz");
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
-export function systemUnderTest(): boolean {
-  return true;
+export function fizzbuzz(n: number): string {
+  if (n % 15 == 0) {
+    return "FizzBuzz";
+  }
+  if (n % 3 == 0) {
+    return "Fizz";
+  }
+  if (n % 5 == 0) {
+    return "Buzz";
+  }
+  return n.toString();
 }
