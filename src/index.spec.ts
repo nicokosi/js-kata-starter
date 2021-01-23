cb42cb86a46fd8f3d8d2bbe21e65652d94b75d4f
Nicolas Kosinski <nicokosi@yahoo.com>
2021-01-23 13:57:04 +0100

    Refactor the tests: use parameterized tests

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,26 +1,26 @@
 import { fizzbuzz } from ".";
 
-describe("FizzBuzz", function () {
-  it("should pass", function () {
-    expect(fizzbuzz(1)).toEqual("1");
-    expect(fizzbuzz(2)).toEqual("2");
-    expect(fizzbuzz(3)).toEqual("Fizz");
-    expect(fizzbuzz(4)).toEqual("4");
-    expect(fizzbuzz(5)).toEqual("Buzz");
-    expect(fizzbuzz(6)).toEqual("Fizz");
-    expect(fizzbuzz(7)).toEqual("7");
-    expect(fizzbuzz(8)).toEqual("8");
-    expect(fizzbuzz(9)).toEqual("Fizz");
-    expect(fizzbuzz(10)).toEqual("Buzz");
-    expect(fizzbuzz(11)).toEqual("11");
-    expect(fizzbuzz(12)).toEqual("Fizz");
-    expect(fizzbuzz(13)).toEqual("13");
-    expect(fizzbuzz(14)).toEqual("14");
-    expect(fizzbuzz(15)).toEqual("FizzBuzz");
-    expect(fizzbuzz(16)).toEqual("16");
-    expect(fizzbuzz(17)).toEqual("17");
-    expect(fizzbuzz(18)).toEqual("Fizz");
-    expect(fizzbuzz(19)).toEqual("19");
-    expect(fizzbuzz(20)).toEqual("Buzz");
-  });
+test.each([
+  [1, "1"],
+  [2, "2"],
+  [3, "Fizz"],
+  [4, "4"],
+  [5, "Buzz"],
+  [6, "Fizz"],
+  [7, "7"],
+  [8, "8"],
+  [9, "Fizz"],
+  [10, "Buzz"],
+  [11, "11"],
+  [12, "Fizz"],
+  [13, "13"],
+  [14, "14"],
+  [15, "FizzBuzz"],
+  [16, "16"],
+  [17, "17"],
+  [18, "Fizz"],
+  [19, "19"],
+  [20, "Buzz"]
+])('fizzbuzz for %i should be "%s"', async (input, expected) => {
+  expect(fizzbuzz(input)).toBe(expected);
 });
