c2adb54ebca52134313f2e1ccf86ca1752f268f5
Nicolas Kosinski <nicokosi@yahoo.com>
2021-01-23 13:43:23 +0100

    Refactor: use the ternary operator.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,6 @@
 export function fizzbuzz(n: number): string {
-  if (n % 15 == 0) {
-    return "FizzBuzz";
-  }
-  if (n % 3 == 0) {
-    return "Fizz";
-  }
-  if (n % 5 == 0) {
-    return "Buzz";
-  }
-  return n.toString();
+  return n % 15 == 0 ? "FizzBuzz" :
+    n % 3 == 0 ? "Fizz" :
+      n % 5 == 0 ? "Buzz" :
+        n.toString();
 }
